08d66607c225a17c0f937e775076fac7a0c550d9
Sanvinganza <anton.shvetsov@mail.ru>
2023-10-17 15:38:30 +0300

    added mobile styles for queues

--- src/pages/Objectives/index.tsx
+++ src/pages/Objectives/index.tsx
@@ -1,16 +1,35 @@
+import { useState } from "react"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 import { Queue } from "./Queue"
 
 export function Objectives() {
+  const [showQueue, setShowQueue] = useState(true)
+
   return (
     <div className="tasks-container">
       {["Queue", "Development", "Done"].map(title => {
         return (
           <div key={title} className="column">
-            <div className="title">{title}</div>
+            <div className="title">
+              <div className="text">{title}</div>
+              <div
+                className="dropdown"
+                onClick={() => setShowQueue(!showQueue)}
+              >
+                <img
+                  src={
+                    showQueue
+                      ? require("../../images/dropup.png")
+                      : require("../../images/dropdown.png")
+                  }
+                  alt=""
+                  className="icon dropdown"
+                />
+              </div>
+            </div>
             <DndProvider backend={HTML5Backend}>
-              <Queue />
+              {showQueue ? <Queue /> : null}
             </DndProvider>
           </div>
         )
