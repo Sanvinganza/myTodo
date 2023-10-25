fe4c15146cd4c7b7449bc7e1ae664fe1038cc5f5
Sanvinganza <anton.shvetsov@mail.ru>
2023-10-25 18:56:00 +0300

    added reload persistans with localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,32 @@ import { configureStore } from "@reduxjs/toolkit"
 import { rootReducer } from "./reducers"
 import { IComment, IProject, ISubtask, ITask } from "./types"
 
+function saveToLocalStorage(state: any) {
+  try {
+    const serialisedState = JSON.stringify(state)
+    localStorage.setItem("persistantState", serialisedState)
+  } catch (e) {
+    console.warn(e)
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState")
+    if (serialisedState === null) return undefined
+    return JSON.parse(serialisedState)
+  } catch (e) {
+    console.warn(e)
+    return undefined
+  }
+}
+
 export const store = configureStore({
   reducer: rootReducer,
+  preloadedState: loadFromLocalStorage(),
 })
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export interface IState {
   projects: IProject[]
